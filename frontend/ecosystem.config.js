53528eaeb59b9a7c1e10f21b24390e1b6cd2ab11
Your Name <you@example.com>
2026-01-17 12:28:15 +0000

    Fixes 3

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -21,7 +21,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      key: '~/.ssh/id_ed25519.pub',
+      key: '~/.ssh/id_ed25519',
       'post-deploy': 'cd frontend && npm i && npm run build',
     },
   },
