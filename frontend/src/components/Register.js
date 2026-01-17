c7ee8d0205584ae9de6695b670bcc224320e2a2e
Jiasechiru <jias2301@gmail.com>
2026-01-17 19:00:12 +0300

    test

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Register ({ onRegister }){
+function Register({ onRegister }) {
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
 
-  function handleSubmit(e){
+  function handleSubmit(e) {
     e.preventDefault();
     const userData = {
       email,
@@ -21,16 +21,16 @@ function Register ({ onRegister }){
           <label className="auth-form__input">
             <input type="text" name="email" id="email"
               className="auth-form__textfield" placeholder="Email"
-              onChange={e => setEmail(e.target.value)} required  />
+              onChange={e => setEmail(e.target.value)} required />
           </label>
           <label className="auth-form__input">
             <input type="password" name="password" id="password"
               className="auth-form__textfield" placeholder="Пароль"
-              onChange={e => setPassword(e.target.value)} required  />
+              onChange={e => setPassword(e.target.value)} required />
           </label>
         </div>
         <div className="auth-form__wrapper">
-          <button className="auth-form__button" type="submit">Зарегистрироваться</button>
+          <button className="auth-form__button" type="submit">Зарегистрироваться-------</button>
           <p className="auth-form__text">Уже зарегистрированы? <Link className="auth-form__link" to="/signin">Войти</Link></p>
         </div>
       </form>
